ca5cc381363b45762a450f13bf8cc7ba0bcc3785
JT <raikiri24@gmail.com>
2023-07-30 22:32:12 -0400

    completed routes

--- routes/thought_route.js
+++ routes/thought_route.js
@@ -2,33 +2,91 @@ const router = require('express').Router();
 const { Thought } = require('../models/Thought');
 
 //get all thoughts
-router.get('/thought')
+router.get('/thought', async (req, res) => {
+    const thought = await Thought.find({});
+    res.json(thought)
+})
 
 
 //get a single thought by id
-router.get('/thought/:id')
+router.get('/thought/:id', async (req, res) => {
+    const thoughtId = req.params.id
+    const thought = await Thought.findById(thoughtId);
+    return res.json(thought)
+})
 
 
 //post a new thought
-router.post('/thought')
+router.post('/thought', async (req, res) => {
+    const thought = await Thought.create(req.body)
+    res.json(user)
+})
 
 
 //update a thought by id
-router.put('/thought/:id')
+router.put('/thought/:id', async (req, res) => {
+    const thoughtId = req.params.id
+    const updatedThoughtData = req.body
+    const updateThought = await Thought.findByIdAndUpdate(
+        thoughtId, updatedThoughtData, {
+        new: true
+    })
+    if (!updateThought) {
+        return res.status(404).json({
+            message: 'unable to update thought'
+        })
+    }
+})
 
 
 //delete thought by id
-router.delete('/thought/:id')
+router.delete('/thought/:id', async (req, res) => {
+    const thoughtId = req.params.id
+    const deleteThought = await Thought.findByIdAndDelete(
+        thoughtId);
+    if (!deleteThought) {
+        return res.status(404).json({
+            message: 'unable to delete user'
+        })
+    }
+    return res.status(200).json({
+        message: 'user deleted successfully'
+    })
+})
 
 
 //create a reaction stored in a single thoughts reaction array field
-router.post('/thought/thoughtId/reactions')
+router.post('/thought/:thoughtId/reactions', async (req, res) => {
+    const { thoughtId } = req.params;
+    const reaction = req.body;
+    const thought = await Thought.findById(thoughtId);
+    if (thought) {
+        thought.reactions.push(reaction);
+        await thought.save();
+        return res.json(thought);
+    } else {
+        return res.status(404).json({
+            message: 'Thought not found'
+        });
+    }
+})
 
 
 //remove a reaction by the reactions reaction Id
-router.delete('/thought/thoughtId/reactions/reactionId')
-
-
-
+router.delete('/thought/:thoughtId/reactions/:reactionId', async (req, res) => {
+    const { thoughtId, reactionId } = req.params;
+    const thought = await Thought.findById(thoughtId);
+    if (thought) {
+        thought.reactions = thought.reactions.filter(
+            reaction => reaction.reactionId !== reactionId
+        );
+        await thought.save();
+        return res.json(thought);
+    } else {
+        return res.status(404).json({
+            message: 'Thought not found'
+        });
+    }
+})
 
 module.exports = router;
--- routes/user_route.js
+++ routes/user_route.js
@@ -2,33 +2,94 @@ const router = require('express').Router();
 const { User } = require('../models/User');
 
 //get all users
-router.get('/user')
+router.get('/user', async (req, res) => {
+    const user = await User.find({});
+    res.json(user)
+})
 
 
 //get a single user by id and populate thought and friend data
-router.get('/user/:id')
+router.get('/user/:id', async (req, res) => {
+    const userId = req.params.id
+    const user = await User.findById(userId)
+        .populate('thoughts')
+        .populate('friends')
+    if (user) {
+        res.json(user)
+    } else {
+        res.json({
+            message: 'user not found'
+        })
+    }
+})
 
 
 //post a new user
-router.post('/user')
+router.post('/user', async (req, res) => {
+    const user = await User.create(req.body)
+    res.json(user)
+})
 
 
 //update a user by id
-router.put('/user/:id')
+router.put('/user/:id', async (req, res) => {
+    const userId = req.params.id
+    const updateUserData = req.body
+    const updateUser = await User.findByIdAndUpdate(userId, updateUserData, {
+        new: true
+    });
+    if (!updateUser) {
+        return res.status(404).json({
+            message: 'unable to update user'
+        })
+    }
+})
 
 
 //delete user by id
-router.delete('/user/:id')
+router.delete('/user/:id', async (req, res) => {
+    const userId = req.params.id
+    const deleteUser = await User.findByIdAndDelete(userId);
+    if (!deleteUser) {
+        return res.status(404).json({
+            message: 'unable to delete user'
+        })
+    }
+    return res.status(200).json({
+        message: 'user deleted successfully'
+    })
+})
 
 
 //add a new friend to users friend list
-router.post('/user/:id/friends/:feindId')
+router.post('/user/:id/friends/:feindId', async (req, res) => {
+    const { userId, friendId } = req.params
+    const user = await User.findById(userId)
+    if (user) {
+        user.friends.push(friendId);
+        await user.save();
+        return res.json(user);
+    } else {
+        return res.status(404).json({
+            message: 'User not found'
+        });
+    }
+})
 
 
 //remove a friend from users friend list
-router.delete('/user/:id/friends/:feindId')
-
-
-
+router.delete('/user/:userId/friends/:friendId', async (req, res) => {
+    const { userId, friendId } = req.params;
+    const user = await User.findById(userId);
+    if (user) {
+        user.friends = user.friends.filter(id => id.toString() !== friendId);
+        await user.save();
+        return res.json(user);
+    } else {
+        return res.status(404).json({
+            message: 'User not found'
+        });
+    }
+})
 
 module.exports = router;
